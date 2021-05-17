04aa753e8349a68614c8949d77ec8ab53c002e55
aki <lala.akira@gmail.com>
2021-05-17 09:43:33 +0900

    remove dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Vue, { ComponentOptions, VueConstructor } from 'vue';
-declare type VwrapOptions = {
-    hasSlot?: boolean;
-};
-export declare const vwrap: (name: string, component: ComponentOptions<Vue> | VueConstructor<Vue>, options?: VwrapOptions) => void;
-export {};
